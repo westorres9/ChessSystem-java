4e63ae00f3d163f3512a472a101620ce65d48921
westorres9 <wester.torres9@gmail.com>
2022-03-08 11:51:31 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Welcome chess");
+		System.out.println("Welcome chess game");
+		Position pos = new Position(3,5);
+		System.out.println("Position: " + pos.toString());
 	}
 
 }
